cd4e975b62aad2b363a2980b2fc5e8c292423ab9
Yanling Wang <yanling.psu@gmail.com>
2020-11-17 04:26:10 +0000

    json and pickle code

--- readbinary.py
+++ readbinary.py
@@ -1,5 +1,8 @@
 from sys import argv
 
 with open(argv[1],'rb') as f:
-  data = f.read()
-  print(data.decode())
+  data = f.read(4)
+  while data != b'':
+    i = int.from_bytes(data, 'little', signed=True)
+    print(i)
+    data = f.read(4)
